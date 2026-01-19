aa117523f21eba20e60f236416df1b9b49fc0d90
remodesuza <remodesuza@proton.me>
2026-01-19 19:09:34 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "IObit Advanced SystemCare Ultimate Pro Download: Complete Installation & Activation Tutorial"
+html_title = "Canon Printer ij Setup Guide: How to Use ij.start.canon and http ij start Canon for Easy Canon Printer Setup in Australia or USA"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
